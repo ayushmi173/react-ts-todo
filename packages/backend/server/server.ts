576ea5220bf2623c84143f6146ff5567f46abec8
ayushmi173 <ayushmishra173@gmail.com>
2021-06-13 20:38:16 +0530

    made full fleged working app

--- packages/backend/server/server.ts
+++ packages/backend/server/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 
 import { TodoRouter } from "../routes/todo";
@@ -8,6 +9,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(
   express.urlencoded({
